82c7595004970cd0f090853b4b58451caa80f081
justin-nykiel <justin.nykiel@gmail.com>
2020-09-15 11:09:28 -0500

    combined functions, arrays and objects more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -20,3 +20,21 @@ console.log(receipt[0].price)
 
 const apartmentBuilding = [['larry', 'moe', 'curly'], 'name']
 console.log(apartmentBuilding[0][1])
+
+const knit = () => {
+    return {item: 'scarf', size: '6ft'}
+}
+console.log(knit().item)
+
+const crayonSelector = () => {
+    return crayonBox; 
+}
+console.log(crayonSelector().crayons[0])
+
+const powerButton = () => {
+    return options();
+}
+const options = () => {
+    console.log("select a song")
+}
+powerButton()
